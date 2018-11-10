231a952efa275b75bed3df9db2a70f731db7c4fc
17520155 <17520155@gm.uit.edu.vn>
2018-11-10 21:09:48 +0700

    Map1
    Map1

--- BBTan/Form1.cs
+++ BBTan/Form1.cs
@@ -16,5 +16,19 @@ namespace BBTan
         {
             InitializeComponent();
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Random rnd = new Random();
+            Rectangle[,] rec = new Rectangle[5, 8];
+            Pen red = new Pen(Color.Red);
+            for (int i = 0; i < 5; i++)
+                for (int j = 0; j < 8; j++)
+                {
+                    rec[i, j] = new Rectangle(j * 50 + 2 + j*2, i * 50 + 2 + i*2, 50, 50);
+                    e.Graphics.DrawRectangle(red, rec[i,j]);
+                }
+
+        }
     }
 }
